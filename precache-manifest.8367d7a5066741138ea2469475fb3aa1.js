9053a96b799e27ab5de6a1f7a0880b344b03e985
YuhangXiong <yxiong4@ualberta.ca>
2019-08-09 01:00:50 -0600

    Updates

---
+++ precache-manifest.8367d7a5066741138ea2469475fb3aa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84abd6b9cac045083e414eed62b1a9f6",
+    "url": "/Monster-Cards/index.html"
+  },
+  {
+    "revision": "0693a47c0613fb2ce5b1",
+    "url": "/Monster-Cards/static/css/main.f2a9f765.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/Monster-Cards/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "0693a47c0613fb2ce5b1",
+    "url": "/Monster-Cards/static/js/main.dce5db6c.chunk.js"
+  },
+  {
+    "revision": "7bf72b5919d3e61b1960",
+    "url": "/Monster-Cards/static/js/runtime~main.619323b0.js"
+  }
+]);
